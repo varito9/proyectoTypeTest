2a199387c409af398e31bc07c8a99a23f0aeaac2
Tonn-nnay <tymartinmarin@gmail.com>
2025-10-31 10:46:58 +0100

    MVP 1 - Pendent possibles correccions

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -17,28 +17,33 @@
       <!-- Div on mostrem la informació de la partida (els textos)-->
       <div id="partida">
         <!--Truquem al game Engine i enviem les props que rebrà aquest component-->
-        <GameEngine :socket="socket" :jugador="jugador" :esEspectador="isSpectator" />
+        <GameEngine :socket="socket" :jugador="jugador" :es-espectador="isSpectator" />
       </div>
       <!--Div on mostrem el temps restant de la partida-->
       <div id="tempsRestant">
-        <h1>Temps Restant:{{ tempsRestant }}s</h1>
+        <TempsRestant />
       </div>
       <!--Div on llistem els usuaris de la partida i els accerts i errors d'aquests-->
-      <div id="ranquing"><!-- Aquí anirà el rànquing --></div>
+      <div id="ranquing">
+        <RankingComponent :llista-jug="jugadors" />
+      </div>
     </div>
   </div>
 
   <!-- vista de endgame-->
-  <div v-else-if="vista === 'endGame'"></div>
+  <div v-else-if="vista === 'endGame'">
+    <RankingComponent :llista-jug="jugadors" />
+  </div>
 </template>
 
 <script setup>
 //imports && exports
 import { ref } from 'vue'
+import RankingComponent from './components/RankingComponent.vue'
 import senseConnexio from './components/PreGame/senseConnexio.vue'
 import viewLobby from './components/PreGame/lobby/viewLobby.vue'
-
-//import GameEngine from './components/Game/GameEngine.vue';
+import GameEngine from './components/Game/GameEngine.vue';
+import TempsRestant from './components/Game/TempsRestant.vue'
 
 //variables
 var socket = senseConnexio.socket
